6278e89f11813c973cf333510bf8d56ea6223041
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-02-26 11:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 function add(a, b) {
   return a + b
 }
+
+function substract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function parse(n) {
+  return parseInt(n)
+}
+
+function paese10(n) {
+  return parseInt(n, 10)
+}
+
